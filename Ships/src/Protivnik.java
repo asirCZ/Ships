136c40d97c976ea9867ea2d54011504c91bcb388
Richard Bílek <richard.bilek@volny.cz>
2021-06-07 22:09:52 +0200

    WIP guessing enemy's ships

--- Ships/src/Protivnik.java
+++ Ships/src/Protivnik.java
@@ -13,6 +13,7 @@ import javax.swing.border.EmptyBorder;
 
 public class Protivnik extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	public Protivnik() {
@@ -35,23 +36,77 @@ public class Protivnik extends JFrame {
 		}
 		Random r = new Random();
 		JLabel instrukce = new JLabel();
-		System.out.println(pocetLodi[0]);
 		for (int i = 0; i < pocetLodi.length;) {
+			int pozice = r.nextInt(100);
+			int velikost = Math.abs((i - 5));
+			int y = 0;
+			bln blocked = new bln();
+			blocked.setBln(false);
+			try {
+				for (int x = velikost; x > 0; x--) {
+					if ((ship[pozice - y] || border[pozice - y])) {
+						blocked.setBln(true);
+						break;
+					}
+					y += 10;
+				}
+			} catch (Exception ArrayIndexOutOfBoundsException) {
+				blocked.setBln(true);
+			}
+			int a = 0;
+			if (velikost != 0 && pozice > (velikost - 1) * 10 && blocked.isBln() == false) {
+				for (int z = velikost; z > 0; z--) {
+					ship[pozice - a] = true;
+
+					if ((pozice - a) % 10 != 0) {
+						System.out.println(pozice - a % 10);
+						border[pozice - a - 1] = true;
+					}
+					if ((pozice - a + 1) % 10 != 0) {
+						border[pozice - a + 1] = true;
+					}
+					a += 10;
+
+				}
+
+				try {
+					border[pozice + 10] = true;
+					if ((pozice + 10) % 10 != 0) {
+						border[pozice + 9] = true;
+					}
+					if ((pozice + 11) % 10 != 0) {
+						border[pozice + 11] = true;
+					}
+				} catch (Exception ArrayIndexOutOfBoundsException) {
+
+				}
+
+				try {
+					border[pozice - a] = true;
+					if ((pozice - a) % 10 != 0) {
+						border[pozice - a - 1] = true;
+					}
+					if ((pozice - a + 1) % 10 != 0) {
+						border[pozice - a + 1] = true;
+					}
+				} catch (Exception ArrayIndexOutOfBoundsException) {
+
+				}
+
+				pocetLodi[i]--;
+			}
 			if (pocetLodi[i] == 0) {
 				i++;
 			}
-			int pozice = r.nextInt(100);
-			int velikost = Math.abs((i - 5));
-
 		}
 		instrukce.setText(
 				"                                                           Zde hádáte lodì protivníka                                                           ");
-		getContentPane().add(instrukce);
+
+		// getContentPane().add(instrukce);
 		int CisloTlacitka = 0;
 		for (int row = 0; row < 10; row++) {
 			for (int col = 0; col < 10; col++) {
 				int btn = CisloTlacitka;
-				ship[CisloTlacitka] = false;
 				JButton b = new JButton();
 				b.setName(Integer.toString(CisloTlacitka));
 				b.setBackground(Color.white);
@@ -63,13 +118,53 @@ public class Protivnik extends JFrame {
 				}
 				b.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						if (ship[btn]) {
+							b.setBackground(Color.red);
+							b.setEnabled(false);
+							if ((btn) % 10 != 0) {
+								getContentPane().getComponent(btn - 1).setEnabled(false);
+							}
+							if ((btn + 1) % 10 != 0) {
+								getContentPane().getComponent(btn + 1).setEnabled(false);
+							}
+							boolean closed = false;
+							int q = 10;
+							try {
+								if (!getContentPane().getComponent(btn + q).isEnabled() && ship[btn + q]
+										|| (ship[btn + q] && ship[btn - q])) {
+									do {
+										if (!getContentPane().getComponent(btn + q).isEnabled() && !ship[btn + q]) {
+											closed = true;
+											break;
+										}
+										q += 10;
+									} while (!closed);
+								}
+							} catch (Exception ArrayIndexOutOfBoundsException) {
+								closed = true;
+							}
+							if (closed) {
+								try {
+									getContentPane().getComponent(btn - 10).setEnabled(false);
+									if ((btn - 10) % 10 != 0) {
+										getContentPane().getComponent(btn - 11).setEnabled(false);
+									}
+									if ((btn - 9) % 10 != 0) {
+										getContentPane().getComponent(btn - 9).setEnabled(false);
+									}
+								} catch (Exception ArrayIndexOutOfBoundsException) {
 
+								}
+							}
+
+						}
+						if (!ship[btn]) {
+							getContentPane().getComponent(btn).setEnabled(false);
+						}
 					}
 
 				});
-				if (ship[btn]) {
-					b.setBackground(Color.red);
-				}
+
 				getContentPane().add(b);
 				CisloTlacitka++;
 			}
